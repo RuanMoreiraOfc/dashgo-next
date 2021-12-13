877d472f29bdeb3759fcef41c2558afdda6b460a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-12 22:45:32 -0300

    Replaces Js Files, to Tsx Files

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default Home;
+
+function Home() {
+   return (
+      <div>
+         <Head>
+            <title>Home | Dashgo</title>
+         </Head>
+
+         <h1>Dashgo</h1>
+      </div>
+   );
+}
