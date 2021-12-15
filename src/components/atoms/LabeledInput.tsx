64d7af67b81385826b4fc9eba489831eead2cf86
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-15 01:37:24 -0300

    Mini-update Component, better types export in LabeledInput

--- src/components/atoms/LabeledInput.tsx
+++ src/components/atoms/LabeledInput.tsx
@@ -1,24 +1,43 @@
-import type { InputProps } from '@chakra-ui/react';
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import type {
+   FormControlProps,
+   FormLabelProps,
+   InputProps, //
+} from '@chakra-ui/react';
+import {
+   FormControl,
+   FormLabel,
+   Input, //
+} from '@chakra-ui/react';
+
+import type { PickRequired } from '@~types/pickRequired';
 
 export default LabeledInput;
-export type { Props as LabeledInputProps };
+export type {
+   Props as LabeledInputProps,
+   StyleProps as LabeledInputStyleProps,
+};
 
-type Props = {
+type StyleProps = {
    label?: string;
-} & (InputProps & Required<Pick<InputProps, 'type' | 'name'>>);
+} & InputProps;
+
+type Props = PickRequired<StyleProps, 'type' | 'name'>;
 
 function LabeledInput({ label, ...restProps }: Props) {
    return (
-      <FormControl>
-         {label && <FormLabel>{label}</FormLabel>}
+      <FormControl {...controlStyles}>
+         {label && <FormLabel {...labelStyles}>{label}</FormLabel>}
 
          <Input {...inputStyles} {...restProps} />
       </FormControl>
    );
 }
 
-const inputStyles: InputProps = {
+const controlStyles: FormControlProps = {};
+
+const labelStyles: FormLabelProps = {};
+
+const inputStyles: StyleProps = {
    size: 'lg',
    variant: 'filled',
    bgColor: 'gray.900',
