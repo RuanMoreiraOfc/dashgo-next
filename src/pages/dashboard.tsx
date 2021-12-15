20d488693952bfc9457049a36f1dd7362ec73d8a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-15 19:51:53 -0300

    Create Page, Dashboard as `/dashboard`

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+export default Dashboard;
+
+function Dashboard() {
+   return (
+      <Fragment>
+         <Head>
+            <title>Dashboard | dashgo</title>
+         </Head>
+      </Fragment>
+   );
+}
