83ed33b603dabe35794ec651cecd67f8f76312a3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-15 18:35:30 -0300

    Mini-update Component, better types export in Logo

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -1,29 +1,36 @@
 import type { TextProps } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 
-import type { LinkProps } from '@c-atoms/Link';
+import type { LinkStyleProps } from '@c-atoms/Link';
 import Link from '@c-atoms/Link';
 
-export default Logo;
-export type { Props as LogoProps };
+import type { PickRequired } from '@~types/pickRequired';
 
-type Props = Required<Pick<LinkProps, 'to' | 'title'>>;
+export default Logo;
+export type {
+   Props as LogoProps,
+   StyleProps as LogoStyleProps, //
+};
+
+type StyleProps = LinkStyleProps;
+type Props = PickRequired<StyleProps, 'to' | 'title'>;
 
 function Logo(props: Props) {
    return (
       <Link {...linkStyles} {...props}>
-         <Text {...logoStyles}>
+         <Text {...logoStyles} as='span'>
             dashgo
-            <Text {...logoDetailStyles}>.</Text>
+            <Text {...logoDetailStyles} as='span'>
+               .
+            </Text>
          </Text>
       </Link>
    );
 }
 
-const linkStyles: Partial<LinkProps> = {};
+const linkStyles: LinkStyleProps = {};
 
 const logoStyles: TextProps = {
-   as: 'span',
    fontSize: '3xl',
    fontWeight: 'bold',
    letterSpacing: 'tight',
@@ -31,7 +38,6 @@ const logoStyles: TextProps = {
 };
 
 const logoDetailStyles: TextProps = {
-   as: 'span',
    pl: '1',
    color: 'pink.500',
 };
