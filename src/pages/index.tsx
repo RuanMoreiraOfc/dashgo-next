56b5187c54d927ddad33498058e559c3de4b7be5
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-15 18:35:52 -0300

    Mini-update Page, segregate styles in SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
 
-import { Flex, Stack, Button } from '@chakra-ui/react';
+import type {
+   FlexProps,
+   StackProps,
+   ButtonProps, //
+} from '@chakra-ui/react';
+import {
+   Flex,
+   Stack,
+   Button, //
+} from '@chakra-ui/react';
 
+import type { LabeledInputStyleProps } from '@c-atoms/LabeledInput';
 import LabeledInput from '@c-atoms/LabeledInput';
 
 export default SignIn;
@@ -14,45 +24,58 @@ function SignIn() {
             <title>Entrar | dashgo</title>
          </Head>
 
-         <Flex
-            w='100vw'
-            h='100vh'
-            align='center'
-            justify='center' //
-         >
-            <Flex
-               as='form'
-               maxW={360}
-               w='100%'
-               p='8'
-               borderRadius={8}
-               bg='gray.800'
-               flexDir='column'
-            >
-               <Stack spacing='4'>
+         <Flex {...pageStyles}>
+            <Flex {...formStyles} as='form'>
+               <Stack {...formStackStyles}>
                   <LabeledInput
+                     {...formEmailInputStyles}
                      label='Email'
                      type='email'
-                     name='email' //
+                     name='email'
                   />
 
                   <LabeledInput
+                     {...formPasswordInputStyles}
                      label='Senha'
                      type='password'
-                     name='password' //
+                     name='password'
                   />
 
-                  <Button
-                     type='submit'
-                     size='lg'
-                     mt='6'
-                     colorScheme='pink' //
-                  >
-                     Entrar
-                  </Button>
+                  <Button {...formButtonStyles}>Entrar</Button>
                </Stack>
             </Flex>
          </Flex>
       </Fragment>
    );
 }
+
+const pageStyles: FlexProps = {
+   w: '100vw',
+   h: '100vh',
+   align: 'center',
+   justify: 'center',
+};
+
+const formStyles: FlexProps = {
+   maxW: 360,
+   w: '100%',
+   p: '8',
+   borderRadius: 8,
+   bg: 'gray.800',
+   flexDir: 'column',
+};
+
+const formStackStyles: StackProps = {
+   spacing: '4',
+};
+
+const formEmailInputStyles: LabeledInputStyleProps = {};
+
+const formPasswordInputStyles: LabeledInputStyleProps = {};
+
+const formButtonStyles: ButtonProps = {
+   type: 'submit',
+   size: 'lg',
+   mt: '6',
+   colorScheme: 'pink',
+};
