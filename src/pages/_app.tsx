032b22fe4885f673421122525b37c7d41dc15f09
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2021-12-15 19:53:03 -0300

    Mini-update Page, add header in `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { theme } from '@styles/theme';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import { ChakraProvider } from '@chakra-ui/react';
+
+import Header from '@c-templates/Header';
 
 export default MyApp;
 
 function MyApp({ Component, pageProps }: AppProps) {
+   const { pathname } = useRouter();
+
    return (
       <ChakraProvider theme={theme}>
-         <Component {...pageProps} />
+         {pathname !== '/' && <Header />}
+
+         <main>
+            <Component {...pageProps} />
+         </main>
       </ChakraProvider>
    );
 }
