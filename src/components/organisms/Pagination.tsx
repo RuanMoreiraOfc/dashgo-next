5b8ed30f2df26ecfb57d496618200e9ce3663c1b
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-01-04 14:47:55 -0300

    Create Component, Pagination as organism

---
+++ src/components/organisms/Pagination.tsx
@@ -0,0 +1,71 @@
+import type {
+   StackProps,
+   BoxProps,
+   TextProps, //
+} from '@chakra-ui/react';
+import {
+   HStack,
+   Box,
+   Text, //
+} from '@chakra-ui/react';
+
+import type {
+   PaginationBarProps,
+   PaginationBarStyleProps,
+} from '@c-molecules/PaginationBar';
+import PaginationBar from '@c-molecules/PaginationBar';
+
+import type { PickRequired } from '@~types/pickRequired';
+
+export default Pagination;
+export type {
+   Props as PaginationProps,
+   StyleProps as PaginationStyleProps, //
+};
+
+type StyleProps = {
+   maxItems?: number;
+} & Pick<PaginationBarStyleProps, 'goto' | 'currentPage' | 'maxPage'> &
+   StackProps;
+type Props = PickRequired<
+   StyleProps,
+   'goto' | 'maxItems' | 'currentPage' | 'maxPage'
+>;
+
+function Pagination({
+   goto,
+   maxItems,
+   currentPage,
+   maxPage,
+   ...restProps
+}: Props) {
+   const maxItemsPerPage = Math.ceil(maxItems / maxPage);
+   const maxItemsLastPage = Math.floor(maxPage * ((maxItems / maxPage) % 1));
+
+   const startPoint = maxItemsPerPage * (currentPage - 1) + 1;
+   const endPoint =
+      currentPage < maxPage ? maxItemsPerPage * currentPage : maxItems;
+
+   return (
+      <HStack {...paginationStyles} {...restProps}>
+         <Box>
+            <Text>
+               <Text as='strong'>{startPoint}</Text>
+               {/* */} - {/* */}
+               <Text as='strong'>{endPoint}</Text>
+               {/* */} de {/* */}
+               <Text as='strong'>{maxItems}</Text>
+            </Text>
+         </Box>
+
+         <PaginationBar {...{ goto, currentPage, maxPage }} />
+      </HStack>
+   );
+}
+
+const paginationStyles: StyleProps = {
+   w: '100%',
+   align: 'center',
+   justify: 'space-between',
+   spacing: '6',
+};
