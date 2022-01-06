a6af1a4c8452225e7548796782b0aa4fc32b2544
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-01-06 10:36:16 -0300

    Mini-update Component, responsiveness improved in Header

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,9 +1,7 @@
-import type {
-   BoxProps, //
-} from '@chakra-ui/react';
-import {
-   Box, //
-} from '@chakra-ui/react';
+import { useBreakpointValue } from '@chakra-ui/react';
+
+import type { BoxProps } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import type { LimitedContainerStyleProps } from '@c-atoms/LimitedContainer';
 import LimitedContainer from '@c-atoms/LimitedContainer';
@@ -23,6 +21,9 @@ type StyleProps = LimitedContainerStyleProps;
 type Props = Omit<StyleProps, 'as' | 'children'>;
 
 function Header(props: Props) {
+   const isSmallVersion = useBreakpointValue({ base: true, md: false });
+   const isMediumVersion = useBreakpointValue({ base: true, lg: false });
+
    return (
       <LimitedContainer {...headerStyles} {...props} as='header'>
          <Box {...logoBoxStyles}>
@@ -33,10 +34,14 @@ function Header(props: Props) {
             />
          </Box>
 
-         <SearchBar {...searchBarStyles} />
+         <SearchBar
+            {...searchBarStyles}
+            hidden={isMediumVersion} //
+         />
 
          <UserActions
             {...userActionsStyles}
+            hideProfileInfo={isSmallVersion}
             name='Ruan Moreira'
             email='ruan_moreira@email.dev'
             avatarUrl='//github.com/ruanmoreiraofc.png'
@@ -49,11 +54,12 @@ const headerStyles: StyleProps = {
    h: '20',
    pt: '4',
    align: 'center',
+   justify: { base: 'space-between', lg: 'unset' },
+   gap: '8',
 };
 
 const logoBoxStyles: BoxProps = {
-   w: '64',
-   mr: '6',
+   mr: { lg: '36' },
 };
 
 const logoStyles: LogoStyleProps = {};
@@ -61,6 +67,5 @@ const logoStyles: LogoStyleProps = {};
 const searchBarStyles: SearchBarStyleProps = {};
 
 const userActionsStyles: UserActionsStyleProps = {
-   pl: '8',
    ml: 'auto',
 };
